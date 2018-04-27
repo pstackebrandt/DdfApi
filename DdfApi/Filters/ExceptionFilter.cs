239f942f4190e46b3fc26c67135d1140f8e7a0b4
Peter Stackebrandt <peter.stackebrandt@googlemail.com>
2018-04-27 10:39:39 +0200

    incl. unvollständiger Exception Fileter

---
+++ DdfApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DdfApi.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        /// <inheridoc/>
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ContentResult()
+            {
+            //    var contentResult = new ContentResult()
+            //    {
+            //        StatusCode = (int)System.Net.HttpStatusCode.InternalServerError,
+            //        Content = _environment.IsDevelopment
+            //"Ups :  ("
+            //    }
+            }
+        }
+    }
+}
